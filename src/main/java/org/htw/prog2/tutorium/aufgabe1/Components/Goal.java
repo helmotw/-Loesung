2a9cbca2c7fdf7e4a13fc8d619bb7b8dcefd0386
s0582080 <102999393+helmotw@users.noreply.github.com>
2022-10-10 23:24:40 +0200

    1.1 fix output

--- src/main/java/org/htw/prog2/tutorium/aufgabe1/Components/Goal.java
+++ src/main/java/org/htw/prog2/tutorium/aufgabe1/Components/Goal.java
@@ -1,12 +1,8 @@
 package org.htw.prog2.tutorium.aufgabe1.Components;
 
-import java.util.Random;
-
 public class Goal {
     private String icon;
     private int xposition, yposition;
-
-    private static Random rand = new Random();
 
     public Goal(int x, int y) {
         icon = " *";
